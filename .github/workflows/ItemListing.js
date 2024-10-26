a279401da1b0e38edc222766f23d71dc8a4ad719
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-26 22:00:22 +0530

    Create ItemListing.js

---
+++ .github/workflows/ItemListing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/ItemListing.css';
+
+function ItemListing() {
+  return (
+    <div className="item-listing">
+      <h2>List Your Artwork</h2>
+      <p>Add artwork details, bidding time, and view bids from buyers.</p>
+    </div>
+  );
+}
+
+export default ItemListing;
