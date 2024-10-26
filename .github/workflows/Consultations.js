c7f92ec83df299275add163969748a9e3d4a6b72
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-26 22:01:59 +0530

    Create Consultations.js

---
+++ .github/workflows/Consultations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Consultations.css';
+
+function Consultations() {
+  return (
+    <div className="consultations">
+      <h2>Book Consultations</h2>
+      <p>Manage calendar bookings and available consultation times.</p>
+    </div>
+  );
+}
+
+export default Consultations;
