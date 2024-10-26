5efe88d7875d698b35420ddd28aaed982ac45007
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-27 02:08:43 +0530

    Delete .github/workflows/urls.py

--- .github/workflows/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register, buyer_home, seller_home
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('buyer_home/', buyer_home, name='buyer_home'),
-    path('seller_home/', seller_home, name='seller_home'),
-]
