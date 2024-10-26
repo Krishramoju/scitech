9ac765de442505f3d32a49c9318ada24cb1c2794
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-26 22:00:49 +0530

    Create Commissions.js

---
+++ .github/workflows/Commissions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Commissions.css';
+
+function Commissions() {
+  return (
+    <div className="commissions">
+      <h2>Commissions</h2>
+      <p>Receive commission requests and send previews for approval.</p>
+    </div>
+  );
+}
+
+export default Commissions;
