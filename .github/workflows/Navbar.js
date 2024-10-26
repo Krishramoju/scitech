908617cb5d3b504de9838f7fcb0dba635ecf9d7a
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-26 21:52:33 +0530

    Create Navbar.js

---
+++ .github/workflows/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Art Marketplace</h1>
+      <button>Logout</button>
+    </nav>
+  );
+}
+
+export default Navbar;
