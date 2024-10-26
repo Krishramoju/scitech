2b4132a412957cf98bc6f61ad178a5b08159311d
Krishramoju <71651407+Krishramoju@users.noreply.github.com>
2024-10-26 22:01:26 +0530

    Create Workshops.js

---
+++ .github/workflows/Workshops.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Workshops.css';
+
+function Workshops() {
+  return (
+    <div className="workshops">
+      <h2>Host Workshops</h2>
+      <p>Set up workshop dates, times, and available slots.</p>
+    </div>
+  );
+}
+
+export default Workshops;
